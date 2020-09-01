cbf6ac8c17d302acdfed9b954c2494eb413ec780
Jose Manuel Garcia O <manuelobezo@gmail.com>
2020-09-01 17:33:05 -0500

    Solve word-reverser lab
    Signed-off-by: Jose Manuel Garcia O <manuelobezo@gmail.com>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(){
-    // Place your magic here
-    return 0;
+void main()
+{
+    int i = 0, f = 0;
+    char word[1000], final[100000]; 
+    while((word[i] = getchar())!= EOF){
+            if(word[i]==' '){
+                for(int y=i-1;y>=0;y--){
+                    final[f]=word[y];
+                    f++;
+                }
+                final[f]=' ';
+                f++;
+                i=0;
+                word[i]='\0';
+            }
+            else if(word[i]=='\n'){
+                for(int y=i-1;y>=0;y--){
+                    final[f]=word[y];
+                    f++;
+                }
+                final[f++]='\n';
+                final[f]='\0';
+                printf("%s", final);
+                i=0;
+                word[i]='\0';
+                f=0;
+                final[f]='\0';
+            }else{
+                i++;
+                word[i]='\0';
+            }
+            
+    }
 }
