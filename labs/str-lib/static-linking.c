49f0c9001f75163c884b49551b7206c8818dbfe3
Jose Manuel Garcia O <manuelobezo@gmail.com>
2020-09-08 23:38:54 -0500

    Solve str-lib lab

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char**argv) {
+    if(argc==4){
+        if(!strcmp(argv[1],"-add")){
+            int iniLength=0,newLength=0;
+            iniLength=mystrlen(argv[2]);
+            char *newString=mystradd(argv[2],argv[3]);
+            newLength=mystrlen(newString);
+            printf("Initial Lenght      : %d\nNew String          : %s\nNew length          : %d\n",iniLength,newString,newLength);
+        }
+        else if(!strcmp(argv[1],"-find")){
+            int pos;
+            pos=mystrfind(argv[2],argv[3]);
+            if(pos!=-1){
+                printf("['%s'] string was found at [%d] position\n",argv[3],pos);
+            }
+            else{
+                printf("['%s'] string was not found\n",argv[3]);
+            }
+        }
+        else{
+            printf("Invalid argument\n");
+        }
+    }
+    else{
+        printf("Invalid number of arguments\n");
+    }
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,42 @@
-
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int i=0;
+    while(str[i]!='\0'){
+        i++;
+    }
+    return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int newSize=mystrlen(origin)+mystrlen(addition);
+    char *newString=malloc(newSize + 1);
+    int aux=0;
+    for(int i=0;origin[i]!='\0';i++,aux++){
+        newString[aux]=origin[i];
+    }
+    for(int i=0;addition[i]!='\0';i++,aux++){
+        newString[aux]=addition[i];
+    }
+    newString[newSize]='\0';
+    return newString;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int aux=0, encontrado=1;
+    int l=mystrlen(substr);
+    for(int i=0;origin[i]!='\0';i++,aux++){
+        aux=0;
+        for(int j=i,k=0;origin[j]!='\0' && substr[k]!='\0';j++,k++){
+            if(origin[j]!=substr[k]){
+                break;
+            }
+            else{
+                aux++;
+            }
+        }
+        if(aux==l)
+        return i;
+    }
+    return -1;
 }
