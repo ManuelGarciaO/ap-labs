5d298bd841eaea97e99df702b417bc95912f341e
Jose Manuel Garcia O <manuelobezo@gmail.com>
2020-09-22 14:20:32 -0500

    Solve go-slices-maps lab
    Signed-off-by: Jose Manuel Garcia O <manuelobezo@gmail.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	var m map[string]int
+	m = make(map[string]int)
+	words := strings.Split(s, " ")
+	for i := 0; i < len(words); i++ {
+		m[words[i]]++
+	}
+	return m
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -3,7 +3,16 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
-     return [][]uint8{}
+	arr := make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		arr[i] = make([]uint8, dx)
+	}
+	for i := 0; i < dy; i++ {
+		for j := 0; j < dx; j++ {
+			arr[i][j] = uint8(i^j)
+		}
+	}	
+	return arr
 }
 
 func main() {
