eee7bc25bdaef939264833978c7447ca88636154
Jose Manuel Garcia O <manuelobezo@gmail.com>
2020-09-29 23:57:52 -0500

    solve advanced-logger lab

--- labs/advanced-logger/logger.c
+++ labs/advanced-logger/logger.c
@@ -1,23 +1,110 @@
 #include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <syslog.h>
 #include "logger.h"
+
+#define RESET		0
+#define BRIGHT 		1
+#define DIM		2
+#define UNDERLINE 	3
+#define BLINK		4
+#define REVERSE		7
+#define HIDDEN		8
+
+#define BLACK 		0
+#define RED		1
+#define GREEN		2
+#define YELLOW		3
+#define BLUE		4
+#define MAGENTA		5
+#define CYAN		6
+#define	WHITE		7
+
+int log_stdout = 1;
 
 int initLogger(char *logType) {
     printf("Initializing Logger on: %s\n", logType);
+    if(!strcmp(logType, "syslog")) {
+        log_stdout = 0;
+    } 
     return 0;
 }
 
+void textcolor(int attr, int fg, int bg)
+{	char command[13];
+
+	/* Command is the control command to the terminal */
+	sprintf(command, "%c[%d;%d;%dm", 0x1B, attr, fg + 30, bg + 40);
+	printf("%s", command);
+}
+
 int infof(const char *format, ...) {
+    textcolor(BRIGHT, GREEN, BLACK);
+    va_list arg;
+	va_start(arg, format);
+    if(log_stdout==1) {
+	    vprintf(format, arg);
+    } 
+    else {
+        openlog("SYSLOG", 0, 0);
+        vsyslog(LOG_INFO, format, arg);
+        closelog();
+    }
+    va_end(arg);
+    textcolor(RESET, WHITE, BLACK);
     return 0;
 }
 
 int warnf(const char *format, ...) {
+    textcolor(BRIGHT, YELLOW, BLACK);
+    va_list arg;
+	va_start(arg, format);
+    if(log_stdout==1) {
+	    vprintf(format, arg);
+    } 
+    else {
+        openlog("SYSLOG", 0, 0);
+        vsyslog(LOG_WARNING, format, arg);
+        closelog();
+    }
+    va_end(arg);
+    textcolor(RESET, WHITE, BLACK);
     return 0;
 }
 
 int errorf(const char *format, ...) {
+    textcolor(BRIGHT, MAGENTA, BLACK);
+    va_list arg;
+	va_start(arg, format);
+    if(log_stdout==1) {
+	    vprintf(format, arg);
+    } 
+    else {
+        openlog("SYSLOG", 0, 0);
+        vsyslog(LOG_ERR, format, arg);
+        closelog();
+    }
+    va_end(arg);
+    textcolor(RESET, WHITE, BLACK);
     return 0;
 }
 int panicf(const char *format, ...) {
+    textcolor(BRIGHT, RED, BLACK);
+    va_list arg;
+	va_start(arg, format);
+    if(log_stdout==1) {
+	    vprintf(format, arg);
+    } 
+    else {
+        openlog("SYSLOG", 0, 0);
+        vsyslog(LOG_EMERG, format, arg);
+        closelog();
+    }
+    va_end(arg);
+    textcolor(RESET, WHITE, BLACK);
     return 0;
 }
 
--- labs/advanced-logger/testLogger.c
+++ labs/advanced-logger/testLogger.c
@@ -3,33 +3,37 @@
 #include "logger.h"
 
 int main(int argc, char **argv){
-
-    printf("%d\n", atoi(argv[1]));
-    switch(atoi(argv[1])) {
-    case 1:
-	// default logging
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
-    case 2:
-	// stdout logging
-	initLogger("stdout");
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
-    case 3:
-	// syslog logging
-	initLogger("syslog");
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
-    default:
-	errorf("Invalid test case");
-	break;
-    }
+	if(argc>0){
+		printf("%d\n", atoi(argv[1]));
+		switch(atoi(argv[1])) {
+		case 1:
+		// default logging
+		infof("INFO Message %d", 1);
+		warnf("WARN Message %d", 2);
+		errorf("ERROR Message %d", 2);
+		break;
+		case 2:
+		// stdout logging
+		initLogger("stdout");
+		infof("INFO Message %d", 1);
+		warnf("WARN Message %d", 2);
+		errorf("ERROR Message %d", 2);
+		break;
+		case 3:
+		// syslog logging
+		initLogger("syslog");
+		infof("INFO Message %d", 1);
+		warnf("WARN Message %d", 2);
+		errorf("ERROR Message %d", 2);
+		break;
+		default:
+		errorf("Invalid test case");
+		break;
+		}		
+	}
+	else{
+		errorf("Invalid test case");
+	}
 
     return 0;
 }
